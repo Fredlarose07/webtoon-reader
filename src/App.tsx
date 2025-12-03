4f0916cb05285066f8d66686d139ec2f39edd979
Yann <yann.duchateau97@gmail.com>
2025-12-03 16:27:03 +0100

    scroll restoration

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useLocation } from 'react-router-dom'
 import OeuvrePage from './components/OeuvrePage/OeuvrePage'
 import WebtoonReader from './components/WebtoonReader/WebtoonReader'
 import './App.css'
+import { useEffect } from 'react'
 
 function App() {
+
+  const location = useLocation()
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [location.pathname])
+  
   return (
     <div className="App">
       <Routes>
